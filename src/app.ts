4382e6641e73e5dd9817c3cbcd060fc8808ddc1c
Ryuji <hi_ryuji@naver.com>
2025-02-14 17:53:11 +0900

    fix: Add cookie-parser to enable logout functionality

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import authRoute from './routes/authRoute';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: 'http://localhost:5173',
